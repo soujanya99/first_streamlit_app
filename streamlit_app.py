c166aecf7a03b70bd32274570b28ad3762ca8732
soujanya99 <31248296+soujanya99@users.noreply.github.com>
2022-12-21 21:22:31 +0530

    Update streamlit_app.py
    added new code to display data received from fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json()) #just writes data to screen
+
+# to add comment 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# to add comment
+streamlit.dataframe(fruityvice_normalized)
